8abb3dc1909611041499f1b5ba48bdb12e253470
Kyle Haesler <kyle.haesler@gmail.com>
2023-12-20 13:56:29 -0500

    Center aligned header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <h1 style={{ color: "red", fontSize: "40px" }}>YouTube Clone</h1>;
+  return (
+    <h1 style={{ color: "red", textAlign: "center", fontSize: "40px" }}>
+      YouTube Clone
+    </h1>
+  );
 };
 
 export default Header;
